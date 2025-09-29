9b8f92913aab85f9953cdaa1f1c78c083a50a460
ribeiro-dev <romilson.2r@gmail.com>
2025-09-29 13:16:17 -0300

    add: eslint configs

--- eslint.config.js
+++ eslint.config.js
@@ -1,2 +1,11 @@
 import { configApp } from '@adonisjs/eslint-config'
-export default configApp()
+export default configApp({
+  "rules": {
+    "@typescript-eslint/naming-convention": [
+      "error",
+      { selector: 'interface', format: ['PascalCase'], custom: { regex: '^I[A-Z]', match: true } }, // Allows INamingConvention
+    ]
+
+  }
+})
+
