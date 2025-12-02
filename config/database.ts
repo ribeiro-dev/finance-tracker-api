596c0e3b7280e93f704e8d36b36799470a3bc00a
ribeiro-dev <romilson.2r@gmail.com>
2025-12-02 11:37:20 -0300

    add: postgres  database connection

--- config/database.ts
+++ config/database.ts
@@ -1,8 +1,10 @@
 import app from '@adonisjs/core/services/app'
 import { defineConfig } from '@adonisjs/lucid'
 
+const isProduction = process.env.NODE_ENV === 'production'
+
 const dbConfig = defineConfig({
-  connection: 'sqlite',
+  connection: isProduction ? 'postgres' : 'sqlite',
   connections: {
     sqlite: {
       client: 'better-sqlite3',
@@ -10,6 +12,21 @@ const dbConfig = defineConfig({
         filename: app.tmpPath('db.sqlite3'),
       },
       useNullAsDefault: true,
+      migrations: {
+        naturalSort: true,
+        paths: ['database/migrations'],
+      },
+    },
+
+    postgres: {
+      client: 'pg',
+      connection: {
+        connectionString:
+          'postgresql://postgres.xlhqaqzxznvdvfhsnjry:UmaSenhaMuitoFoda@aws-1-sa-east-1.pooler.supabase.com:5432/postgres',
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      },
       migrations: {
         naturalSort: true,
         paths: ['database/migrations'],
